19ad7374af4e581a8e8c446f695d9005bb061d4d
wuff1996 <33193253+wuff1996@users.noreply.github.com>
2020-09-18 14:20:50 +0800

    add findAllKey function
    find all the same key-value

--- json/string/string.go
+++ json/string/string.go
@@ -34,3 +34,39 @@ func FindKeyString(s string, key string) (string, error) {
 	}
 	return "", fmt.Errorf("FindKeyString: can't find %s from %s", key, s)
 }
+
+//find All the same key-value and return a alice of them.
+func FindAllKeyString(s string, key string) ([]string, error) {
+	sliceString := make([]string, 0)
+	if !strings.Contains(s, ",") {
+		if strings.Contains(s, "\""+key+"\"") {
+			t := strings.Split(s, "\"")
+			for i, t2 := range t {
+				if strings.Contains(t2, ":") {
+					if t[i-1] == key {
+						sliceString = append(sliceString, t[i+1])
+						return sliceString, nil
+					}
+				}
+			}
+		}
+	}
+	line := strings.Split(s, ",")
+	for _, v := range line {
+		if strings.Contains(v, "\""+key+"\":") {
+			t := strings.Split(v, "\"")
+			for i, t2 := range t {
+				if strings.Contains(t2, ":") {
+					if t[i-1] == key {
+						sliceString = append(sliceString, t[i+1])
+						break
+					}
+				}
+			}
+		}
+	}
+	if len(sliceString) != 0 {
+		return sliceString, nil
+	}
+	return nil, fmt.Errorf("FindKeyString: can't find %s from %s", key, s)
+}
